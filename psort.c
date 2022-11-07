552dac45b4aad9d287e942854cfef51737403745
LongjieGuo <lguo@royal-22.cs.wisc.edu>
2022-11-06 22:24:03 -0600

    almost done

--- psort.c
+++ psort.c
@@ -20,6 +20,13 @@ struct key_record {
 struct key_record **record_array = NULL;
 int num_procs = 0;
 int n_records = 0;
+
+void print_records() {
+    for (int i = 0; i < n_records; i++) {
+        printf("key: %d\n", record_array[i]->key);
+    }
+}
+
 // variation on geeks for geeks: https://www.geeksforgeeks.org/merge-sort-using-multi-threading/
 void merge(int left, int mid, int right){
     int left_size = mid - left + 1;
@@ -92,60 +99,57 @@ void* merge_sort_thread(void *arg) {
 int main(int argc, char *argv[]) {
     num_procs = get_nprocs();
 
-    printf("good, 1 \n");
     //check if more than 2 arguments
     if (argc != 3) {
         write(STDERR_FILENO, error_message, strlen(error_message));
         exit(1);
     }
-    printf("good, 2 \n");
+    
     //open input_file
     int input_file = open(argv[1], O_RDONLY);
     if ( input_file == -1) {
         write(STDERR_FILENO, error_message, strlen(error_message));
         exit(1);
     }
-    printf("good, 3 \n");
     //open output_file
     int output_file = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0666);
     if (output_file == -1) {
         write(STDERR_FILENO, error_message, strlen(error_message));
         exit(1);
     }
-    printf("good, 4 \n");
+
     //find number of records
     struct stat file_stat;
     if (stat(argv[1], &file_stat) < 0) {
         write(STDERR_FILENO, error_message, strlen(error_message)); 
         exit(1);
     }
-    int n_records = (file_stat.st_size / 100);// records split by 100 bytes
-    
-    printf("good, 5\n");
+
+    n_records = (file_stat.st_size / 100);// records split by 100 bytes
+
     //initilaize memory for record_array, and its entries
     record_array = (struct key_record**) malloc(n_records * sizeof(struct key_record *));
     for (int i = 0; i < n_records ; i++) {
         record_array[i] = (struct key_record *) malloc(sizeof(struct key_record));
     }
-    printf("good, 6 \n");
 
     //read keys into records_table
+    // printf("file size %d n_records: %d \n", file_stat.st_size, n_records);
     int idx = 0;
-    printf("good, 7 \n");
     char *file_ptr = mmap(NULL, file_stat.st_size, PROT_READ, MAP_PRIVATE, input_file, 0);
     for (int i = 0; i < file_stat.st_size; i+=100) {
        
         // cpy first four bytes for the key, 96 for the record into array. 
-        memcpy((void*)&record_array[idx]->key, file_ptr+i,4);
-        printf("good, 10000 \n");
+        memcpy((void*)&record_array[idx]->key, file_ptr+i, 4);
+        //printf("good, 10000, %d \n", i);
         memcpy(record_array[idx]->record, file_ptr+i+4, 96);
-        printf("good, 20000 \n");
+        //printf("good, 20000, %d \n", i);
         idx++;
-        printf("good, 30000 \n");
+        //printf("good, 30000 \n");
     }
 
+    //print_records();
     
-    /* parallel sorting and merge 
     pthread_t threads[num_procs];
 
     for (int i = 0; i < num_procs; i++) {
@@ -156,40 +160,39 @@ int main(int argc, char *argv[]) {
     }
  
     // merge
-    int thread_idx = 0;
-    int thread_size = record_size / num_procs;
-    int bonus = record_size - thread_size * num_procs;
+    int thread_size = n_records / num_procs;
+    int bonus = n_records - thread_size * num_procs;
  
     for (int i = 0; i < num_procs - 1; i++) {
         int left = 0;
-        int mid = (thread_idx + 1) * thread_size - 1;
-        int right = (thread_idx + 2) * thread_size - 1;
+        int mid = (i + 1) * thread_size - 1;
+        int right = (i + 2) * thread_size - 1;
 
         if (i == num_procs - 2) {
             right += bonus;
-        }        
+        }
+        //  printf("%d %d %d\n", left, mid, right);        
         merge(left, mid, right);
     }
-    */ 
     
-    printf("good, 8 \n");
+    //print_records();
+
+
+    //printf("good, 8 \n");
     //write the sorted record_array entries to an output_file 
     for (int i = 0; i < n_records; i++) {
         write(output_file, record_array[i], sizeof(struct key_record));
     }
 
-
     //close all open files
     close(input_file);
     close(output_file);
-     printf("good, 9 \n");
     //free memory
     for (int i = 0; i < n_records; i++) {
         free(record_array[i]); 
     }
     free(record_array);
 
-    printf("good, 10 \n");
     return 0;
 }
 
